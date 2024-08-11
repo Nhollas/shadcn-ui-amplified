a339bc7275e4956105281ea6402cae3eb35d16df
Nick Hollas <nicholashollas@yahoo.co.uk>
2024-08-11 20:34:55 +0100

    Set threshold to 0.35

--- src/playwright/date-picker.e2e.ts
+++ src/playwright/date-picker.e2e.ts
@@ -2,7 +2,7 @@ import { expect, takeSnapshot } from "@chromatic-com/playwright"
 
 import test from "@/playwright/fixtures/next-fixture"
 test.describe("Date Picker", () => {
-  test.use({ diffThreshold: 0.2 })
+  test.use({ diffThreshold: 0.35 })
 
   test("Date Picker - Regression", async ({ page }, testInfo) => {
     await page.goto("/v9")
