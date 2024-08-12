330b5ac76a11a2190cf3b3c6278bb168122dc657
Nick Hollas <nicholashollas@yahoo.co.uk>
2024-08-12 16:11:18 +0100

    prefer size class to H and W

--- src/app/(routes)/v9/calendar.tsx
+++ src/app/(routes)/v9/calendar.tsx
@@ -47,7 +47,7 @@ function Calendar({
             : "aria-selected:rounded-md",
         ),
         day_button:
-          "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 w-8 p-0 font-normal",
+          "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground size-8 p-0 font-normal",
         range_start: "day-range-start",
         range_end: "day-range-end",
         selected:
