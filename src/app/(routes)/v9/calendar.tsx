ab3c1ad5986d564bcdb16732ac994f815ea5a533
Nick Hollas <nicholashollas@yahoo.co.uk>
2024-08-12 12:50:56 +0100

    Final few tweaks.

--- src/app/(routes)/v9/calendar.tsx
+++ src/app/(routes)/v9/calendar.tsx
@@ -41,23 +41,23 @@ function Calendar({
           "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
         week: "flex w-full mt-2",
         day: cn(
-          buttonVariants({ variant: "ghost" }),
-          "size-8 relative p-0 text-center text-sm focus-within:relative focus-within:z-20 rounded-md",
+          "group relative p-0 text-center text-sm focus-within:relative focus-within:z-20 aria-selected:bg-accent [&[aria-selected].day-outside]:bg-accent/50 [&[aria-selected].day-range-end]:rounded-r-md",
           props.mode === "range"
-            ? "[&.day-range-end]:rounded-r-md [&.day-range-start]:rounded-l-md first:aria-selected:rounded-l-md last:aria-selected:rounded-r-md"
+            ? "[&[.day-range-end]:rounded-r-md [&[.day-range-start]:rounded-l-md first:aria-selected:rounded-l-md last:aria-selected:rounded-r-md"
             : "aria-selected:rounded-md",
         ),
-        day_button: "size-8 rounded-md",
+        day_button:
+          "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 w-8 p-0 font-normal group-aria-selected:opacity-100",
         range_start: "day-range-start",
         range_end: "day-range-end",
         selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        today: "bg-accent text-accent-foreground",
+          "day-selected [&>button]:aria-selected:bg-primary [&>button]:aria-selected:text-primary-foreground [&>button]:hover:bg-primary [&>button]:hover:text-primary-foreground [&>button]:aria-selected:focus:bg-primary [&>button]:aria-selected:focus:text-primary-foreground",
+        today: "[&>button]:bg-accent [&>button]:text-accent-foreground",
         outside:
-          "day-outside text-muted-foreground opacity-50  aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
+          "day-outside [&>button]:text-muted-foreground [&>button]:!opacity-50 [&>button]:aria-selected:!bg-accent/50 [&>button]:aria-selected:!text-muted-foreground [&>button]:aria-selected:!opacity-30",
         disabled: "text-muted-foreground opacity-50",
         range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "[&>button]:aria-selected:bg-accent [&>button]:aria-selected:text-accent-foreground",
         hidden: "invisible",
         ...classNames,
       }}
