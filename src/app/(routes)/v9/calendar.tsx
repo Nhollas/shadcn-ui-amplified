ac5131ad03b0dfb22a411db4c3588ba2d8646274
Nick Hollas <nicholashollas@yahoo.co.uk>
2024-08-12 16:13:53 +0100

    Re-use buttonVariants.

--- src/app/(routes)/v9/calendar.tsx
+++ src/app/(routes)/v9/calendar.tsx
@@ -46,8 +46,10 @@ function Calendar({
             ? "[&.day-range-end]:rounded-r-md [&.day-range-start]:rounded-l-md aria-selected:[&.day-range-end]:rounded-r-md first:aria-selected:rounded-l-md last:aria-selected:rounded-r-md"
             : "aria-selected:rounded-md",
         ),
-        day_button:
-          "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground size-8 p-0 font-normal",
+        day_button: cn(
+          buttonVariants({ variant: "ghost" }),
+          "size-8 p-0 font-normal",
+        ),
         range_start: "day-range-start",
         range_end: "day-range-end",
         selected:
